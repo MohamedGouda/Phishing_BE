9c4581d1e4b72ec5e3afca36510e5334706ded5c
Khaled Esmail Mostafa <khaled.esmail222@gmail.com>
2022-11-22 20:38:36 +0100

    [PF-60] Add users on database level

--- src/Models/user.js
+++ src/Models/user.js
@@ -1,7 +1,22 @@
-exports.User = class Note{
-    constructor(userId , name , email){
-        this.userId = userId;
-        this.name = name;
-        this.email = email;
-    }
-}
+const { Sequelize, DataTypes } = require("sequelize");
+const db = require("../Database/connection");
+
+const Users = db.define(
+  "user",
+  {
+    user_id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: DataTypes.TEXT,
+    email: DataTypes.TEXT,
+    domain: DataTypes.ARRAY([DataTypes.STRING]),
+    password: DataTypes.TEXT,
+    subscription_date: DataTypes.DATE,
+    subscription_expiry_date: DataTypes.DATE,
+  },
+  { timestamps: false }
+);
+
+module.exports = Users;
